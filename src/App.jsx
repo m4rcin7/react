28af26a10f7f77b1537b8f68b3c30ef4fbe3dd2a
mkpage1337 <mrku1337@gmail.com>
2025-02-27 14:03:30 +0100

    :commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+`;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Markdown Previewer</h1>
+        <div className="boxes-container">
+          <textarea name="editor" id="editor"></textarea>
+          <div id="preview"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
