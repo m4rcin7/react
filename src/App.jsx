21f23554fd1d3900b54851c5690bb2867c3ac513
mkpage1337 <mrku1337@gmail.com>
2025-02-28 16:23:07 +0100

    :commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,77 @@
-const defaultMarkdown = `
-# Welcome to my React Markdown Previewer!
-
-## This is a sub-heading...
-### And here's some other cool stuff:
-
-Heres some code, \`<div></div>\`, between 2 backticks.
-
-\`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
-
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... **_both!_**
-
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
-
-![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
-
-- And of course there are lists.
-  - Some are bulleted.
-      - With different indentation levels.
-        - That look like this.
-`;
+import { useState, useEffect } from "react";
+import marked from "marked";
+import "./App.css";
 
 function App() {
+  const [editorContent, setEditorContent] = useState("");
+
+  const defaultMarkdown = `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  | Wild Header | Crazy Header | Another Header? |
+  |-------------|--------------|-----------------|
+  | Your content can | be here, and it | can be here.... |
+  | And here. | Okay. | I think we get it. |
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  `;
+
+  useEffect(() => {
+    setEditorContent(defaultMarkdown);
+  }, [defaultMarkdown]);
+
+  // Update the preview as the editor content changes
+  const handleEditorChange = (event) => {
+    setEditorContent(event.target.value);
+  };
+
   return (
-    <>
-      <div>
-        <h1>Markdown Previewer</h1>
-        <div className="boxes-container">
-          <textarea name="editor" id="editor"></textarea>
-          <div id="preview"></div>
-        </div>
-      </div>
-    </>
+    <div className="app-container">
+      <textarea
+        id="editor"
+        value={editorContent}
+        onChange={handleEditorChange}
+        className="editor"
+      />
+      <div
+        id="preview"
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(editorContent) }}
+      />
+    </div>
   );
 }
 
